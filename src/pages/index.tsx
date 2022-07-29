69613146912d7ced0b856d9d8605e20d7fd7a43f
Reese Armstrong <me@reeseric.ci>
2022-07-29 15:09:05 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description={`${siteConfig.description}`}>
+      description={`${siteConfig.tagline}`}>
       <HomepageHeader />
       <main>
       </main>
