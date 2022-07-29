e3d53287cd74d155eb9cfd6fa359922e7b678d3d
Reese Armstrong <me@reeseric.ci>
2022-07-29 16:21:13 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Midtown Rulers. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
